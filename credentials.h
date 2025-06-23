ea57bcd2ee05f21648c4ba914226acee1775b7dc
Roberto Carriero <66308934+carrieroroberto@users.noreply.github.com>
2025-06-23 19:21:01 +0200

    Add files via upload

---
+++ credentials.h
@@ -0,0 +1,10 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#define WIFI_SSID ""
+#define WIFI_PASS ""
+#define MAC_ADDR "XX:XX:XX:XX:XX:XX"
+#define BOT_TOKEN  ""
+#define ALLOWED_ID ""
+
+#endif
